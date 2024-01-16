37a827ea96a74fe39b62f5001cd16533d59d62e0
Alan <76206945+AlannBerg@users.noreply.github.com>
2024-01-16 13:17:48 +0100

    Update Chrome.py

--- Lab2/Chrome.py
+++ Lab2/Chrome.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import logging
 import time
 from selenium.common.exceptions import TimeoutException
